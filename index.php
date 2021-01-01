8d495e2929828affadab473491e094c136887e7e
Loghman Avand <avand.loghman@gmail.com>
2021-01-01 21:11:54 +0330

    StupidRouter-Finished

--- index.php
+++ index.php
@@ -1,22 +1,11 @@
 <?php
 # front conroller
 
-use App\Utilities\Url;
-
+use App\Core\StupidRouter;
 include "bootsrap/init.php";
 
 
-$route = Url::current_route();
+$router = new StupidRouter();
+$router->run();
 
-if($route == '/hasan/blue')
-    include BASEPATH . 'views/colors/blue.php';
-
-if($route == '/hasan/red')
-    include BASEPATH . 'views/colors/red.php';
-
-if($route == '/hasan/green')
-    include BASEPATH . 'views/colors/green.php';
-
-if($route == '/atal/matal/totoole')
-    include BASEPATH . 'views/colors/red.php';
 
